9abff1a7f6535df6a896219724fc51310288aa5c
Carlos Guindel <cguindel@ing.uc3m.es>
2017-06-02 12:50:27 +0200

    Add missing nodes

--- src/stereo_pattern.cpp
+++ src/stereo_pattern.cpp
@@ -9,13 +9,13 @@
     the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    velo2cam_calibration is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with velo2cam_calibration.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #define DEBUG 1
